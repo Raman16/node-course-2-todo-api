b3df529664f66f5ab30fe8edddf963daa686b0d3
Raman <bandariraman@gmail.com>
2018-02-12 23:02:33 +0530

    Mongo DB connection with matlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 
-const REMOTE_MONGO = 'mongodb://bandariraman:r2m2642i@123B@ds247357.mlab.com:47357/todoapi';
+const REMOTE_MONGO = 'mongodb://<bandariraman>:<r2m2642i@123B>@ds247357.mlab.com:47357/todoapi';
 const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
 const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
     
