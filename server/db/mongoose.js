4ff6c396af96985d3a0cc51b6b025181616fb829
Raman <bandariraman@gmail.com>
2018-02-12 23:44:11 +0530

    testin g db connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,19 @@
 var mongoose=require('mongoose');
 
-const REMOTE_MONGO = 'mongodb://<bandariraman>:<r2m2642i@123B>@ds247357.mlab.com:47357/todoapi';
+const REMOTE_MONGO = 'mongodb://bandariraman:r2m2642i@123B@ds247357.mlab.com:47357/todoapi';
 const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
-const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+const MONGO_URI = process.env.MONGODB_URI || LOCAL_MONGO;
     
-mongoose.connect(MONGO_URI)
-.then((res)=>{ console.log('connected')})
-.catch((err)=>{ 
-   //fs.appendFileSync('log',`${err}\n`);       
-   console.log(err);
-});
+mongoose.Promise=global.Promise;
+mongoose.connect(MONGO_URI);
+// mongoose.connect(MONGO_URI)
+// .then((res)=>{ console.log('connected')})
+// .catch((err)=>{ 
+//    //fs.appendFileSync('log',`${err}\n`);       
+//    console.log(err);
+// });
+
+module.exports={
+    mongoose
+}
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
